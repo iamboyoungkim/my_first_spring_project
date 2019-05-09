cc848fd8f0a740b142a899ca70a49e2bcb7fd330
김보영 <2015004402@hanyang.ac.kr>
2019-05-09 17:18:27 +0900

    make first simple spring project

--- testPjt/src/main/java/testPjt/MainClass.java
+++ testPjt/src/main/java/testPjt/MainClass.java
@@ -1,10 +1,21 @@
 package testPjt;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 	
 	public static void main(String[] args) {
-		TransportationWalk transportationWalk = new TransportationWalk();
+// 		기존 방법 
+//		TransportationWalk transportationWalk = new TransportationWalk();
+//		transportationWalk.move();
+		
+		// spring container 생성 
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		//	container로부터 객체를 가져옴 
+		TransportationWalk transportationWalk = ctx.getBean("tWalk", TransportationWalk.class);
 		transportationWalk.move();
+		
+		// 닫아줌 
+		ctx.close();
 	}
-
 }
