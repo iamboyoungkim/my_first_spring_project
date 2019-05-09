4d4f0bc837083f4dd59b4be16be80b40213ad480
김보영 <2015004402@hanyang.ac.kr>
2019-05-09 16:53:18 +0900

    add reference folders

---
+++ testPjt/src/main/java/testPjt/MainClass.java
@@ -0,0 +1,10 @@
+package testPjt;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		TransportationWalk transportationWalk = new TransportationWalk();
+		transportationWalk.move();
+	}
+
+}
